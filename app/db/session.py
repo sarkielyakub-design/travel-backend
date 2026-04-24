dfdcc560583129335259a90218024156aec44ffc
your name <you@email.com>
2026-04-24 05:57:57 +0100

    switch to postgres

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./new.db"  # ✅ fresh DB
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}  # 🔥 REQUIRED for Render
 )
 
 SessionLocal = sessionmaker(
