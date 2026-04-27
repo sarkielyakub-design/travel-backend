28c849cff2691f0a32cc031f01194bb4dd57aebe
your name <you@email.com>
2026-04-27 19:33:57 +0100

    callback

--- app/services/payment.py
+++ app/services/payment.py
@@ -14,7 +14,7 @@ def initialize_payment(email: str, amount: int, reference: str):
         "email": email,
         "amount": int(amount * 100),  # 🔥 Paystack uses kobo
         "reference": reference,
-        "callback_url": "http://localhost:3000/payment-success"
+        callback_url: "https://myhamdala-frontend.vercel.app/payment/success"
     }
 
     response = requests.post(url, json=payload, headers=headers)
