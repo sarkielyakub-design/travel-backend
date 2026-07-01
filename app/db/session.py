96d9056ac2f676d836eef56a2aff8fe623f7c35f
your name <you@email.com>
2026-07-01 23:37:44 +0100

    Debug DATABASE_URL

--- app/db/session.py
+++ app/db/session.py
@@ -3,6 +3,14 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set.")
 
 load_dotenv()
 
