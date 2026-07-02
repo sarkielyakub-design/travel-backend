f306e478ae1089a28685f7e163d9b0e728b0e16b
your name <you@email.com>
2026-07-02 01:37:33 +0100

    cloud

--- app/core/cloudinary.py
+++ app/core/cloudinary.py
@@ -1,13 +1,13 @@
 import os
 import cloudinary
 
+print("Cloud Name:", os.getenv("CLOUDINARY_CLOUD_NAME"))
+print("API Key:", os.getenv("CLOUDINARY_API_KEY"))
+print("API Secret Exists:", bool(os.getenv("CLOUDINARY_API_SECRET")))
+
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True,
 )
-
-print("Cloudinary configured")
-print("Cloud:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-print("API Key:", os.getenv("CLOUDINARY_API_KEY"))
